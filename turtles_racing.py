9f8bb4dd573b00baf3014a5ec4e8e9acde371fa0
avisane05 <avisane05.as@gmail.com>
2024-05-14 15:53:50 +0530

    code 1

--- turtles_racing.py
+++ turtles_racing.py
@@ -1,3 +1,10 @@
+import turtle
+import time
+import random
+
+WIDTH, HEIGHT = 500, 500
+COLORS = ['red', 'green', 'blue', 'orange', 'yellow', 'black', 'purple', 'pink', 'brown', 'cyan']
+
 def get_number_of_racers():
     racers = 0
     while True:
@@ -13,4 +20,15 @@ def get_number_of_racers():
         else:
             print("Number is not in range 2 -10. Try again.")
 
+
+def init_turtle():
+    screen = turtle.Screen()
+    screen.setup(WIDTH, HEIGHT)
+    screen.title("Turtle Racing!")
+
+racers = get_number_of_racers()
+init_turtle()
+
+random.shuffle(COLORS)
+colors = COLORS[:racers]
 
